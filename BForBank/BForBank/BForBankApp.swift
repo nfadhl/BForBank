8df85465384fecead362013365b2369bf3b20509
Fadhl Nader <>
2024-10-29 15:35:57 +0100

    init project

---
+++ BForBank/BForBank/BForBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BForBankApp.swift
+//  BForBank
+//
+//  Created by Fadhl Nader on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct BForBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
